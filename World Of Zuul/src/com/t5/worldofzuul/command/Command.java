6c60c8e247fdc7a4f71cb768e972646391553de3
Camilla <71691759+Shoujiin@users.noreply.github.com>
2020-11-05 14:09:07 +0100

    Update Command.java

--- World Of Zuul/src/com/t5/worldofzuul/command/Command.java
+++ World Of Zuul/src/com/t5/worldofzuul/command/Command.java
@@ -106,7 +106,7 @@ public class Command {
 
     private void printHelp(Player player) {
         System.out.println("You are lost. You are alone. You wander");
-        System.out.println("around at the university.");
+        System.out.println("around the fallen forrest.");
         System.out.println();
         System.out.println("Your command words are:");
         player.getParser().showCommands();
