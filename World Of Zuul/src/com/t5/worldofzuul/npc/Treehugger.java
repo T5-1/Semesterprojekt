c6e222957db3020a6b69797d4437f7f6afa8939d
Camilla <71691759+Shoujiin@users.noreply.github.com>
2020-11-05 12:08:19 +0100

    Update Treehugger

--- World Of Zuul/src/com/t5/worldofzuul/npc/Treehugger.java
+++ World Of Zuul/src/com/t5/worldofzuul/npc/Treehugger.java
@@ -3,7 +3,10 @@ package com.t5.worldofzuul.npc;
 
 public class Treehugger extends NPC {
     public Treehugger() {
-        super("Treehugger", "?");
+        super("Treehugger", "Hey dude, you want some of this organic oil for the roots? Man the world is\n" +
+                "so corrupted, we need more love in the world.. more trees. I’m Olive Bluebird and I can like, \n" +
+                "totally help you with this quest, man. \n" +
+                "*****insert information*****");
     }
 
 }
