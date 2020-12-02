b4c4b9f59bfd35ac8cd7d04a305f65a01db4ef71
Malthe75 <70543194+Malthe75@users.noreply.github.com>
2020-12-02 17:25:25 +0100

    Made cave bigger

--- World Of Zuul/src/dk/t5/grp1/worldofzuul/room/Cave.java
+++ World Of Zuul/src/dk/t5/grp1/worldofzuul/room/Cave.java
@@ -1,5 +1,6 @@
 package dk.t5.grp1.worldofzuul.room;
 
+import dk.t5.grp1.worldofzuul.Game;
 import dk.t5.grp1.worldofzuul.assets.CaveEntity;
 import dk.t5.grp1.worldofzuul.item.Seed;
 import dk.t5.grp1.worldofzuul.npc.Bat;
@@ -12,6 +13,6 @@ public class Cave extends Room {
     public Cave(String description, String name) {
         super(description, name, new Seed(), new ArrayList<>(), new Bat(1000, 350), "/textures/rooms/cave.png");
 
-        super.assets.add(new CaveEntity(1200, 150));
+        super.assets.add(new CaveEntity(Game.width-270, 256));
     }
 }
