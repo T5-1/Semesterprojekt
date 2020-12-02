a37bd930999a68d37602f338d21c814f4a94f982
OliverDahlWagner <71690615+OliverDahlWagner@users.noreply.github.com>
2020-12-02 13:04:26 +0100

    npc placement

--- World Of Zuul/src/dk/t5/grp1/worldofzuul/room/Desert.java
+++ World Of Zuul/src/dk/t5/grp1/worldofzuul/room/Desert.java
@@ -6,6 +6,6 @@ import dk.t5.grp1.worldofzuul.npc.Cactus;
 public class Desert extends Room{
 
     public Desert(String description, String name) {
-        super(description, name, new Sun(), new Cactus(40, 300), "/textures/rooms/desert.png");
+        super(description, name, new Sun(), new Cactus(1150, 670), "/textures/rooms/desert.png");
     }
 }
