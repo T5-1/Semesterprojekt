b3bfea1e299fa17b61d8beab3cecf1f3155f6d86
TheixOfficial <emillind8@gmail.com>
2020-12-02 17:04:46 +0100

    Fixed Deforester text

--- World Of Zuul/src/dk/t5/grp1/worldofzuul/npc/Deforester.java
+++ World Of Zuul/src/dk/t5/grp1/worldofzuul/npc/Deforester.java
@@ -4,7 +4,7 @@ import dk.t5.grp1.worldofzuul.graphics.Sprite;
 
 public class Deforester extends NPC {
     public Deforester(int x, int y) {
-        super("De-forester Dennis", "/res/text/dialogue/deforester.txt", x, y, Sprite.npcDeforester, x - 20, y - 20, x + 20, y + 20);
+        super("De-forester Dennis", "res/text/dialogue/deforester.txt", x, y, Sprite.npcDeforester, x - 20, y - 20, x + 20, y + 20);
     }
 
 
